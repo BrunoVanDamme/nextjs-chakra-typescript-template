2abc115da405863d230de65621a56a74bf410f49
Bruno Van Damme <vof.heights@gmail.com>
2021-02-27 21:59:29 +0100

    Chakra installation

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,13 @@
+import React from 'react'
+import { Container } from '@/components/Container'
+
 export default function Home() {
   return (
-    <div className="mt-5 text-lg">Starter template with ES Linter, Prettier, Chakra and Typescript configured.</div>
-  );
+    <Container height="100vh">
+      <div className="mt-5 text-lg">
+        Starter template with ES Linter, Prettier, Chakra and Typescript
+        configured.
+      </div>
+    </Container>
+  )
 }
